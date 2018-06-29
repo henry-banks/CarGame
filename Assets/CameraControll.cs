f3610e791944a7f94c3c55606197a76e923e9ebc
Benjamin Vongtawan <7319bvongtawan@gmail.com>
2018-06-29 16:09:12 -0700

    Added camera controller

---
+++ Assets/CameraControll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControll : MonoBehaviour
+{
+    PlayerController playerController;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerController = FindObjectOfType<PlayerController>();
+
+        offset = transform.position - playerController.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        transform.position = playerController.transform.position + offset;
+	}
+
+    void Update()
+    {
+        transform.rotation = playerController.transform.rotation;
+    }
+}
