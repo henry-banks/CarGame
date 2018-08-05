634a9b24a86468ff75d780bdf07e139c3640b5c1
Henry Banks <mrshmid@gmail.com>
2018-08-04 17:09:47 -0700

    i didn't actually do anything...

---
+++ Assets/Scripts/Destroyable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//A car part that can be damaged and destroyed.
+
+//https://answers.unity.com/questions/767962/want-velocity-along-a-direction.html
+//Velocity calculation help.
+public class Destroyable : MonoBehaviour {
+
+
+    public float health;
+    //How fast the object has to be going in a direction before exploding.
+    public float hitThreshold;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.relativeVelocity.magnitude > hitThreshold)
+    }
+}
---
+++ Assets/Scripts/Exploder.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Handles all exploding thingos.  pretty simple atm.
+public class Exploder : MonoBehaviour {
+
+    public GameObject explosion;
+
+	public void Explode()
+    {
+        Instantiate(explosion, gameObject.transform.position, gameObject.transform.rotation);
+	}
+}
